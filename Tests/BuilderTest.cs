5e2e2243bb89c60d425ce1421d8e04f3080e714a
sowmyatellapragada <63146223+sowmyatellapragada@users.noreply.github.com>
2020-12-22 19:24:06 +0530

    Add files via upload

---
+++ Tests/BuilderTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LINQ;
+
+namespace LINQ
+{
+    [TestClass]
+    public class BuilderTest
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public void BuildIntegerSequenceTest()
+        {
+            var listBuilder = new Builder();
+
+            var result = listBuilder.BuildIntegerSequence();
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void BuildStringSequenceTest()
+        {
+            var listBuilder = new Builder();
+
+            var result = listBuilder.BuildStringSequence();
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void CompareSequenceTest()
+        {
+            var listBuilder = new Builder();
+            var result = listBuilder.CompareSequences();
+
+            foreach (var item in result)
+                TestContext.WriteLine(item.ToString());
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
